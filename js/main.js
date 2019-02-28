44302e4674d449581a39b5ff4daf3493b9c9920a
Accessibilite <acessibilite.paris@outlook.fr>
2019-02-28 12:29:58 +0100

    js

--- js/main.js
+++ js/main.js
@@ -28,9 +28,9 @@
 	var fullHeight = function() {
 
 		if ( !isMobile.any() ) {
-			$('.js-fullheight').css('height', $(window).height());
+			$('.js-fullheight').css('height', $(window).height()- 300);
 			$(window).resize(function(){
-				$('.js-fullheight').css('height', $(window).height());
+				$('.js-fullheight').css('height', $(window).height()-300);
 			});
 		}
 
